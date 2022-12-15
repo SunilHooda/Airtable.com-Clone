7fe8457c7d262e2ba701a9b27e795628f34c33f9
AviAndhariya <avi.andhariya@gmail.com>
2022-12-16 01:14:59 +0530

    Pricing page UI by Avi

--- airtable-clone/src/App.js
+++ airtable-clone/src/App.js
@@ -2,15 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Footer from './Components/LP_Footer';
-import Pricing from './Components/Pricing';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Pricing /> 
-      {/* <Footer/> */}
+ 
+      <Footer/>
     </div>
   );
 }
